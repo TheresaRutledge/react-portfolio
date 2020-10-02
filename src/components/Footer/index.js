15667ff5868d4ee66565a59df13eb762564d8a4d
Theresa Rutledge <t.roserutledge@gmail.com>
2020-10-02 12:40:33 -0700

    footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,9 +3,12 @@ import React from 'react';
 
 const Footer = () => {
     return (
-<footer className='footer'>
-        <p>Contact Me</p>
-    </footer>
+        <footer className='footer'>
+            <a href='tel:925-785-6151'>Phone</a>
+            <a href='mailto:t.roserutledge@gmail.com'>Email</a>
+            <a href='https://github.com/TheresaRutledge' target='_blank'>GitHub</a>
+            <a href='https://www.linkedin.com/in/theresarutledge/' target='_blank'>LinkedIn</a>
+        </footer>
     )
 }
 
