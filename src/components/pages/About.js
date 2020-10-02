bd0163fd957f6e75e36d5228517eb222473c495c
Theresa Rutledge <t.roserutledge@gmail.com>
2020-10-02 11:20:04 -0700

    project content

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,16 +1,19 @@
 import React from "react";
 
+
 const About = () => (
     <div className='content-container'>
         <section className='section-content'>
             <div className='content'>
-                <h1>About Me</h1>
+                <h2>About Me</h2>
+                <div class='profile-image-container'>
+                    <img class='profile-image' src={require('../../assets/profile-pic.jpeg')} alt='Theresa headshot'></img>
+               </div>
                 <p>
-                    Nunc pharetra finibus est at efficitur. Praesent sed congue diam. Integer gravida dui mauris,
-                    ut interdum nunc egestas sed. Aenean sed mollis diam. Nunc aliquet risus ac finibus porta. Nam
-                    quis arcu non lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-                    imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus in. Sed rhoncus mollis
-                    diam, sit amet facilisis lectus blandit at.
+                Hi! I'm Theresa and I'm currently learning skills to become a web developer! My background so far has been as a sales executive selling software. All my time working with technology
+                        led me to realize that I wanted to learn how to create not just sell. I'm familiar with HTML, CSS and JavaScript. More skills to come as I continue to learn!
+                    
+                I live in the San Francisco Bay area with my husband and two cats. When I'm not at a computer I'm hiking, knitting or making fun crafts with my Cricut. 
                 </p>
             </div>
         </section>
--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,21 +1,79 @@
 import React from "react";
 
-const Projects = () => (
-    <div className='content-container'>
-    <section className='section-content'>
-    <div className='content'>
-        <h1>My Projects</h1>
-        <p>
-            Nunc pharetra finibus est at efficitur. Praesent sed congue diam. Integer gravida dui mauris,
-            ut interdum nunc egestas sed. Aenean sed mollis diam. Nunc aliquet risus ac finibus porta. Nam
-            quis arcu non lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-            imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus in. Sed rhoncus mollis
-            diam, sit amet facilisis lectus blandit at.
-</p>
-</div>
-    </section>
 
-</div>
-);
+
+function importAll(r) {
+    let images = {};
+    r.keys().map((item, index) => { images[item.replace('./', '')] = r(item); });
+    return images;
+}
+
+const images = importAll(require.context('../../assets/project-images', false, /\.(gif|jpe?g|svg)$/));
+
+const Projects = () => {
+
+    const projectList = [
+        {
+            name: 'Cosmopolitan Tastes',
+            image: 'cosmotastes.jpeg',
+            url: 'http://cosmopolitan-tastes.herokuapp.com',
+            githubUrl: 'https://github.com/TheresaRutledge/CosmopolitanTastes'
+        },
+        {
+            name: 'Wine and Dine',
+            image:'wine-dine.jpg',
+            url:'https://theresarutledge.github.io/wine-and-dine/',
+            githubUrl:'https://github.com/TheresaRutledge/wine-and-dine'
+        },
+        {
+            name:'Run Buddy',
+            image:'run-buddy.jpg',
+            url:'https://theresarutledge.github.io/run-buddy/',
+            githubUrl:'https://github.com/TheresaRutledge/run-buddy'
+        },
+        {
+            name:'Daily Planner',
+            image:'daily-planner.jpeg',
+            url:'https://theresarutledge.github.io/daily-planner/',
+            githubUrl:'https://github.com/TheresaRutledge/daily-planner'
+        },
+        {
+            name:'Weather',
+            image:'weather.jpeg',
+            url:'https://theresarutledge.github.io/weather-dashboard/',
+            githubUrl:'https://github.com/TheresaRutledge/weather-dashboard'
+        },
+        {
+            name:'Secret Santa',
+            image:'secret-santa.jpg',
+            url:'https://theresarutledge.github.io/secret-santa/',
+            githubUrl:'https://github.com/TheresaRutledge/secret-santa'
+        },
+    ];
+
+
+    return (
+        <div className='content-container'>
+            <section className='section-content'>
+                <div className='content'>
+                    {
+                        projectList.map((project) => (
+                            <div className='project-container'>
+                                <img className='project' src={images[project.image]} alt={project.name}></img>
+                                <div className='project-links'>
+                                    <a href={project.url} className='project-link project-name' target='_blank' >{project.name}</a>
+                                    <a href={project.githubUrl} className='project-link' target='_blank'>
+                                        <img src={require('../../assets/GitHub-Mark-32px.png')}></img>
+                                    </a>
+                                </div>
+                            </div>
+                        ))}
+
+                </div>
+            </section>
+
+        </div>
+    )
+};
 
 export default Projects;
