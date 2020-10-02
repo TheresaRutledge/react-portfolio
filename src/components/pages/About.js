751fc4851f25b082269642e254d47561ad3dd098
Theresa Rutledge <t.roserutledge@gmail.com>
2020-10-02 11:30:18 -0700

    About me

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -6,15 +6,17 @@ const About = () => (
         <section className='section-content'>
             <div className='content'>
                 <h2>About Me</h2>
-                <div class='profile-image-container'>
+                <div className='row'>
+                <div class='profile-image-container col'>
                     <img class='profile-image' src={require('../../assets/profile-pic.jpeg')} alt='Theresa headshot'></img>
                </div>
-                <p>
+                <p className='about-me-content col'>
                 Hi! I'm Theresa and I'm currently learning skills to become a web developer! My background so far has been as a sales executive selling software. All my time working with technology
                         led me to realize that I wanted to learn how to create not just sell. I'm familiar with HTML, CSS and JavaScript. More skills to come as I continue to learn!
                     
                 I live in the San Francisco Bay area with my husband and two cats. When I'm not at a computer I'm hiking, knitting or making fun crafts with my Cricut. 
                 </p>
+                </div>
             </div>
         </section>
     </div>
