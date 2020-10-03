87377df94c80f152b8f6d13ce97d6f25d5b916a6
Theresa Rutledge <t.roserutledge@gmail.com>
2020-10-03 10:22:16 -0700

    nav highlighted for active page

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,19 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 function Header(props) {
   const navTabs = ['About', 'Projects', 'Contact', 'Resume'];
+
   return (
     <div className='header'>
       <h1>Theresa Rutledge</h1>
       <ul className="list">
         {navTabs.map(tab => (
-          <li className="list-item" key={tab}>
+          <li className={`${props.currentPage===tab ? 'list-item navActive' : 'list-item'}`} key={tab}>
             <a
               href={'#' + tab.toLowerCase()}
               onClick={() => props.setCurrentPage(tab)}
-              className={
-                props.currentPage === tab ? 'nav-link active' : 'nav-link'
-              }
+              className='list-item'
+              // {
+              //   props.currentPage === tab ? 'list-item active' : 'nav-link'
+              // }
             >
               {tab}
             </a>
