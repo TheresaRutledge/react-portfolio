79fe98270f2ffeb2eb2cb6b9872bdf153b8e5e44
Theresa Rutledge <t.roserutledge@gmail.com>
2020-10-04 09:14:40 -0700

    Updates

---
+++ precache-manifest.460ff95d875441bf601bf234347461c5.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "998bf3a430413895b68cf77805da776a",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "d0b2d9e235d25a459760",
+    "url": "/react-portfolio/static/css/main.4dd52d26.chunk.css"
+  },
+  {
+    "revision": "e3298e40531eaea9f879",
+    "url": "/react-portfolio/static/js/2.e69461fd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-portfolio/static/js/2.e69461fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0b2d9e235d25a459760",
+    "url": "/react-portfolio/static/js/main.fd35e904.chunk.js"
+  },
+  {
+    "revision": "aff33cef9dbb8787d6b3",
+    "url": "/react-portfolio/static/js/runtime-main.213b1880.js"
+  },
+  {
+    "revision": "d37f2588b058099c6cb127a05ee5f273",
+    "url": "/react-portfolio/static/media/Rutledge_Theresa.d37f2588.pdf"
+  },
+  {
+    "revision": "af2f188380fe2e1fb947237a4a7cdd95",
+    "url": "/react-portfolio/static/media/background.af2f1883.jpg"
+  },
+  {
+    "revision": "56dd158fdc1e7df9c91707298e47ce23",
+    "url": "/react-portfolio/static/media/profile-pic.56dd158f.jpeg"
+  },
+  {
+    "revision": "3adfa0c73972c6e28a818a0009140582",
+    "url": "/react-portfolio/static/media/run-buddy.3adfa0c7.jpg"
+  },
+  {
+    "revision": "0da493aa12d1490dd60a7231ae044130",
+    "url": "/react-portfolio/static/media/secret-santa.0da493aa.jpg"
+  },
+  {
+    "revision": "c6b7889685521b1ae4df3f80dce7a291",
+    "url": "/react-portfolio/static/media/wine-dine.c6b78896.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/TheresaRutledge/react-portfolio/precache-manifest.4fdb1688a6423b1de970f58ddbb87610.js"
+  "/react-portfolio/precache-manifest.460ff95d875441bf601bf234347461c5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/TheresaRutledge/react-portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
